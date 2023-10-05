a16430e318b2b3582d14323dfba6a79f21372a0b
Alexandr Petrovich <apalion97@gmail.com>
2023-10-05 18:31:01 +0300

    Commit 2

---
+++ src/components/Rating/Rating.tsx
@@ -0,0 +1,14 @@
+import {Star} from "./Star";
+import React from "react";
+
+export function Rating() {
+    return (
+        <div>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+        </div>
+    )
+}
